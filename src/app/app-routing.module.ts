096e2a70693a590b65aa9afe779a1a4618b0a13e
Fernando <fernandobroccox1@gmail.com>
2023-02-02 16:24:53 -0300

    Implementação dos rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CurriculoComponent } from './curriculo/curriculo.component';
+import { ContatoComponent } from './contato/contato.component';
+import { PortifolioComponent } from './portifolio/portifolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+/*{path: '', redirectTo: '', pathMatch: 'full',}*/
+
+{path: ' contato', component: ContatoComponent},
+{path: 'curriculo', component: CurriculoComponent},
+{path: 'portifolio', component: PortifolioComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/portifolio/portifolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portifolio',
+  templateUrl: './portifolio.component.html',
+  styleUrls: ['./portifolio.component.css']
+})
+export class PortifolioComponent {
+
+}
