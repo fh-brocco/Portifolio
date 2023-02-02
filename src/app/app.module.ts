4499c1220adfdbbb03423209ccbb88a1efe7a117
Fernando <fernandobroccox1@gmail.com>
2023-02-02 16:05:18 -0300

    Implementação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { ContatoComponent } from './contato/contato.component';
+import { CurriculoComponent } from './curriculo/curriculo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    ContatoComponent,
+    CurriculoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/curriculo/curriculo.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CurriculoComponent } from './curriculo.component';
+
+describe('CurriculoComponent', () => {
+  let component: CurriculoComponent;
+  let fixture: ComponentFixture<CurriculoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CurriculoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CurriculoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
